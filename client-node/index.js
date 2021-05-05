e851eaaccdf3015ed1cef89394af7144a6aaec6d
Deepak <sah.sslpu@gmail.com>
2021-05-05 17:54:49 +0530

    add node client

---
+++ client-node/index.js
@@ -0,0 +1,27 @@
+var PROTO_PATH = __dirname + '/../task/task.proto';
+console.log(PROTO_PATH)
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+// Suggested options for similarity to existing grpc.load behavior
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+// The protoDescriptor object has the full package hierarchy
+var task = protoDescriptor.task.Task;
+
+var tc = new task('localhost:59690', grpc.credentials.createInsecure());
+
+var taskRequest = {name: "new important task"}
+tc.Create(taskRequest, function(err, feature) {
+    if(err) {
+        console.log("error")
+    } else {
+        console.log("task created")
+    }
+})
